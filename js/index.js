c0a1cfa5307630d7d6c9578f44743c4a3a243571
narkeeta <cmccabe4363@gmail.com>
2016-09-29 01:09:55 -0700

    update

--- js/index.js
+++ js/index.js
@@ -1,54 +1,25 @@
 $(function () { // $(document).ready shorthand
-	$('h13').fadeIn(2000);
+	$('h13').fadeIn(1000);
 });
 $(document).ready(function () {
 	$(window).scroll(function () {
 		/* Check the location of each desired element */
-		$('.hideme').each(function (i) {
-			var bottom_of_object = $(this).position().top + $(this).height();
+		$('.aniUp').each(function (i) {
+			var bottom_of_object = $(this).position().top;
 			var bottom_of_window = $(window).scrollTop() + $(window).height();
 			/* If the object is completely visible in the window, fade it it */
 			if (bottom_of_window > bottom_of_object) {
 				$(this).animate({
 					'opacity': '1',
-				}, 1500);
-			}
-		});
-	});
-	
-	$(window).scroll(function () {
-		/* Check the location of each desired element */
-		$('.aniRight').each(function (i) {
-			var bottom_of_object = $(this).position().top + $(this).height();
-			var bottom_of_window = $(window).scrollTop() + $(window).height();
-			/* If the object is completely visible in the window, fade it it */
-			if (bottom_of_window > bottom_of_object) {
-				$(this).animate({
-					'opacity': '1',
-					'right': '0',
-				}, 1500);
-			}
-		});
-	});
-	
-	$(window).scroll(function () {
-		/* Check the location of each desired element */
-		$('.aniLeft').each(function (i) {
-			var bottom_of_object = $(this).position().top + $(this).height();
-			var bottom_of_window = $(window).scrollTop() + $(window).height();
-			/* If the object is completely visible in the window, fade it it */
-			if (bottom_of_window > bottom_of_object) {
-				$(this).animate({
-					'opacity': '1',
-					'left': '0',
-				}, 1500);
+					'top': '0',
+				}, 1000);
 			}
 		});
 	});
 });
 
 
-$(".poster").on("click", function() {
+$(".scroll-right").on("click", function() {
   if ($(".poster-left").is(':first-child')) {
     $(".poster").last().addClass("templeft");
   } else $(".poster-left").prev().addClass("templeft");
@@ -62,6 +33,32 @@ $(".poster").on("click", function() {
   if ($(".poster-right").is(':first-child')) {
     $(".poster").last().addClass("tempright");
   } else $(".poster-right").prev().addClass("tempright");
+  $(".poster-right").removeClass("poster-right");
+  
+  $(".tempright").addClass("poster-right");
+  $(".tempright").removeClass("tempright");
+  
+  $(".tempcenter").addClass("poster-center");
+  $(".tempcenter").removeClass("tempcenter");
+  
+  $(".templeft").addClass("poster-left");
+  $(".templeft").removeClass("templeft");
+});
+
+$(".scroll-left").on("click", function() {
+  if ($(".poster-left").is(':last-child')) {
+    $(".poster").first().addClass("templeft");
+  } else $(".poster-left").next().addClass("templeft");
+  $(".poster-left").removeClass("poster-left");
+
+  if ($(".poster-center").is(':last-child')) {
+    $(".poster").first().addClass("tempcenter");
+  } else $(".poster-center").next().addClass("tempcenter");
+  $(".poster-center").removeClass("poster-center");
+
+  if ($(".poster-right").is(':last-child')) {
+    $(".poster").first().addClass("tempright");
+  } else $(".poster-right").next().addClass("tempright");
   $(".poster-right").removeClass("poster-right");
   
   $(".tempright").addClass("poster-right");
