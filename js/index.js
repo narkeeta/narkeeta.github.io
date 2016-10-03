3c405b9445fd3cd10a3e58b0bf67ccb5cab9194a
narkeeta <cmccabe4363@gmail.com>
2016-10-02 20:27:50 -0700

    Style Update

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-$(function () { // $(document).ready shorthand
+$(function () {
 	$('h13').fadeIn(1000);
 });
 
-$(document).ready(function(){       
-   var scroll_start = 0;
-   var startchange = $('#scroll-starter');
-   var offset = startchange.offset();
-   $(document).scroll(function() { 
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
-          $('#menu').css('background-color', 'rgba(255,255,255,.8)').css('color', 'black');
-       } else {
-          $('#menu').css('background-color', 'transparent');
-       }
-   });
+$(document).ready(function () {
+	var scroll_start = 0;
+	var startchange = $('#scroll-starter');
+	var offset = startchange.offset();
+	$(document).scroll(function () {
+		scroll_start = $(this).scrollTop();
+		if (scroll_start > offset.top) {
+			$('#menu').css('background-color', 'rgba(255,255,255,.8)').css('color', 'black');
+		} else {
+			$('#menu').css('background-color', 'transparent').css('color', 'white');
+		}
+	});
 });
 
 $(document).ready(function () {
@@ -31,61 +31,72 @@ $(document).ready(function () {
 			}
 		});
 	});
+
+	$("#btn-GIT").click(function () {
+		$("#contact-sponsor").hide();
+		$("#contact-message").fadeIn(1000);
+	});
+
+	$("#btn-BAS").click(function () {
+		$("#contact-message").hide();
+		$("#contact-sponsor").fadeIn(1000);
+	});
 });
 
+$(".scroll-right").on("click", function () {
+	if ($(".poster-left").is(':first-child')) {
+		$(".poster").last().addClass("templeft");
+	} else $(".poster-left").prev().addClass("templeft");
+	$(".poster-left").removeClass("poster-left");
 
-$(".scroll-right").on("click", function() {
-  if ($(".poster-left").is(':first-child')) {
-    $(".poster").last().addClass("templeft");
-  } else $(".poster-left").prev().addClass("templeft");
-  $(".poster-left").removeClass("poster-left");
+	if ($(".poster-center").is(':first-child')) {
+		$(".poster").last().addClass("tempcenter");
+	} else $(".poster-center").prev().addClass("tempcenter");
+	$(".poster-center").removeClass("poster-center");
 
-  if ($(".poster-center").is(':first-child')) {
-    $(".poster").last().addClass("tempcenter");
-  } else $(".poster-center").prev().addClass("tempcenter");
-  $(".poster-center").removeClass("poster-center");
+	if ($(".poster-right").is(':first-child')) {
+		$(".poster").last().addClass("tempright");
+	} else $(".poster-right").prev().addClass("tempright");
+	$(".poster-right").removeClass("poster-right");
 
-  if ($(".poster-right").is(':first-child')) {
-    $(".poster").last().addClass("tempright");
-  } else $(".poster-right").prev().addClass("tempright");
-  $(".poster-right").removeClass("poster-right");
-  
-  $(".tempright").addClass("poster-right");
-  $(".tempright").removeClass("tempright");
-  
-  $(".tempcenter").addClass("poster-center");
-  $(".tempcenter").removeClass("tempcenter");
-  
-  $(".templeft").addClass("poster-left");
-  $(".templeft").removeClass("templeft");
+	$(".tempright").addClass("poster-right");
+	$(".tempright").removeClass("tempright");
+
+	$(".tempcenter").addClass("poster-center");
+	$(".tempcenter").removeClass("tempcenter");
+
+	$(".templeft").addClass("poster-left");
+	$(".templeft").removeClass("templeft");
 });
 
-$(".scroll-left").on("click", function() {
-  if ($(".poster-left").is(':last-child')) {
-    $(".poster").first().addClass("templeft");
-  } else $(".poster-left").next().addClass("templeft");
-  $(".poster-left").removeClass("poster-left");
+$(".scroll-left").on("click", function () {
+	if ($(".poster-left").is(':last-child')) {
+		$(".poster").first().addClass("templeft");
+	} else $(".poster-left").next().addClass("templeft");
+	$(".poster-left").removeClass("poster-left");
 
-  if ($(".poster-center").is(':last-child')) {
-    $(".poster").first().addClass("tempcenter");
-  } else $(".poster-center").next().addClass("tempcenter");
-  $(".poster-center").removeClass("poster-center");
+	if ($(".poster-center").is(':last-child')) {
+		$(".poster").first().addClass("tempcenter");
+	} else $(".poster-center").next().addClass("tempcenter");
+	$(".poster-center").removeClass("poster-center");
 
-  if ($(".poster-right").is(':last-child')) {
-    $(".poster").first().addClass("tempright");
-  } else $(".poster-right").next().addClass("tempright");
-  $(".poster-right").removeClass("poster-right");
-  
-  $(".tempright").addClass("poster-right");
-  $(".tempright").removeClass("tempright");
-  
-  $(".tempcenter").addClass("poster-center");
-  $(".tempcenter").removeClass("tempcenter");
-  
-  $(".templeft").addClass("poster-left");
-  $(".templeft").removeClass("templeft");
+	if ($(".poster-right").is(':last-child')) {
+		$(".poster").first().addClass("tempright");
+	} else $(".poster-right").next().addClass("tempright");
+	$(".poster-right").removeClass("poster-right");
+
+	$(".tempright").addClass("poster-right");
+	$(".tempright").removeClass("tempright");
+
+	$(".tempcenter").addClass("poster-center");
+	$(".tempcenter").removeClass("tempcenter");
+
+	$(".templeft").addClass("poster-left");
+	$(".templeft").removeClass("templeft");
 });
 
-var shiftWindow = function() { scrollBy(0, -60) };
+var shiftWindow = function () {
+	scrollBy(0, -60)
+};
 if (location.hash) shiftWindow();
 window.addEventListener("hashchange", shiftWindow);
