0ce324acf1fe70606235e299c99055c50a69afb9
narkeeta <cmccabe4363@gmail.com>
2016-10-12 20:32:11 -0700

    update

---
+++ mwgallegos/MWG_Folios/assets/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+	var mainImg = $("#LayoutDiv1").children("img").attr("src").replace("myFull", "mytinythumbs");
+	$("#LayoutDiv4").children('a:first').addClass("firsta");
+	$("#LayoutDiv4").children('a:last').addClass("lasta");
+
+
+	if ($("#LayoutDiv4").children('a:first').children("img").attr("src") == mainImg) {
+		var prevLink = $("#LayoutDiv4").children("a:last").attr("href");
+	} else {
+		var prevLink = $("#LayoutDiv4").find('img[src="' + mainImg + '"]').parent("a").prev().attr("href");
+	}
+
+	if ($("#LayoutDiv4").children('a:last').children("img").attr("src") == mainImg) {
+		var nextLink = $("#LayoutDiv4").children("a:first").attr("href");
+	} else {
+		var nextLink = $("#LayoutDiv4").find('img[src="' + mainImg + '"]').parent("a").next().attr("href");
+	}
+
+
+
+	$("#btn-back").attr("href", prevLink);
+	$("#btn-forward").attr("href", nextLink);
+
+	$("#LayoutDiv1").swiperight(function () {
+		window.location.href = prevLink;
+	});
+
+	$("#LayoutDiv1").swipeleft(function () {
+		window.location.href = prevLink;
+	});
+});
