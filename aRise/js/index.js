4cd8c53d30d4d94e41204893886b802df9ce120e
Collin McCabe <narkeetadesign@gmail.com>
2016-11-30 09:22:26 -0700

    Back

--- aRise/js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-	$("#menu-expand-white").click(function () {
-		$("#menu").fadeIn();
-	});
-	$("#menu-collapse").click(function () {
-		$("#menu").fadeOut();
-	});
-});
