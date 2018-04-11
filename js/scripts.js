5f3a0e2208f72df58b9b2925d28675d3e2f72075
narkeeta <narkeetadesign@gmail.com>
2018-04-10 18:38:59 -0700

    Css

--- js/scripts.js
+++ js/scripts.js
@@ -55,3 +55,10 @@ $(document).ready(function () {
 		$("#form1").submit();
 	});
 });
+window.addEventListener("load",function() {
+	// Set a timeout...
+	setTimeout(function(){
+		// Hide the address bar!
+		window.scrollTo(0, 1);
+	}, 0);
+});
