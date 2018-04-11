f300e07c82a80cecd5ba08f97902ae76100f6055
narkeeta <narkeetadesign@gmail.com>
2018-04-11 14:20:37 -0700

    Css

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,12 @@ $(document).ready(function () {
 				$.scrollify.current().addClass("active_item");
 				$(".menu span:nth-child(3)").css('background-color', $.scrollify.current().css('border-color'));
 				$(".menu span:nth-child(3)").attr("ID", $.scrollify.current().data("section-name"));
+			} else if ($.scrollify.currentIndex() < 2) {
+				$(".active_item").removeClass("active_item");
+				$(".portfolio_item:first-child").addClass("active_item")
+			} else {
+				$(".active_item").removeClass("active_item");
+				$(".portfolio_item:last-child").addClass("active_item")
 			}
 			var bColor = $.scrollify.current().css('border-color');
 			$('.cool_border').css('border-color', bColor);
@@ -37,21 +43,11 @@ $(document).ready(function () {
 		},
 	});
 	$(".menu span").click(function() {
-		var toGo = $(this).attr("ID");
-		if (toGo == "contact") {
-			$(".active_item").removeClass("active_item");
-			$(".portfolio_item:last-child").addClass("active_item");
-			$(".menu span:nth-child(3)").attr("ID", $(".portfolio_item:last-child").data("section-name"));
-			$(".menu span:nth-child(3)").css('background-color',  $(".portfolio_item:last-child").css('border-color'));
-		} else if (toGo == "home" || toGo == "about") {
-			$(".active_item").removeClass("active_item");
-			$(".portfolio_item:first-child").addClass("active_item");
-			$(".menu span:nth-child(3)").attr("ID", $(".portfolio_item:first-child").data("section-name"));
-			$(".menu span:nth-child(3)").css('background-color',  $(".portfolio_item:first-child").css('border-color'));
-		}
-		$.scrollify.move("#" + toGo);
+		$.scrollify.move("#" + $(this).attr("ID"));
 	});
 	$("#submit-btn").click(function () {
 		$("#form1").submit();
+	});
+	$(".portfolio_item .images img").click(function() {
 	});
 });
